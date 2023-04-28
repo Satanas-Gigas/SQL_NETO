0e719872c99a857b2af41748b78525f62442ee2e
Satanas-Gigas <120686392+Satanas-Gigas@users.noreply.github.com>
2023-04-28 05:27:31 +0500

    Update SQL_2_2.sql

--- SQL_2_2.sql
+++ SQL_2_2.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS departments (
-	       id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
         name VARCHAR(40) NOT NULL	
 );
 
 CREATE TABLE IF NOT EXISTS employees (
-           id SERIAL PRIMARY KEY,
-         name VARCHAR(40) NOT NULL,
-id_departments INTEGER REFERENCES departments(id),
-   id_managers INTEGER REFERENCES employees(id)
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(40) NOT NULL,
+        id_departments INTEGER REFERENCES departments(id),
+        id_managers INTEGER REFERENCES employees(id)
 );
